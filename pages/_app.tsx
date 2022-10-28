364a51d17aa691c0799235797e777368c6634a01
Paul Faass <paul.faass@pxio.de>
2022-10-28 12:14:31 +0200

    ⭐ feat(analytics): add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-2LWKXYFV8Q"
+        strategy="afterInteractive"
+      ></Script>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
 
-export default MyApp
+          gtag('config', 'G-2LWKXYFV8Q');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
+}
+export default MyApp;
